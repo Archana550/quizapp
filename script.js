42dec3e079a3338ba30644437e1b07be8e85bcfc
Archana550 <51438542+Archana550@users.noreply.github.com>
2021-04-30 00:48:16 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,110 @@
+const quizDB = [
+    {
+        question: "Q1: What is the full form of HTML?",
+        a: "Hello To My Land",
+        b: "Hey Text Markup Language",
+        c: "HyperText Makeup Language",
+        d: "HyperText Markup Language",
+        ans: "ans4"
+    },
+
+    {
+        question: "Q2: What is the full form of CSS?",
+        a: "Cascading Style Sheets",
+        b: "Cream Silk Saree",
+        c: "Cascading Style Shift",
+        d: "Create Style Sheet",
+        ans: "ans1"
+    },
+
+    {
+        question: "Q3: What is the full form of JS?",
+        a: "Java Study",
+        b: "Java Skills",
+        c: "Joker Script",
+        d: "Java Script",
+        ans: "ans4"
+    },
+    {
+        question: "Q4: What is the capital of india?",
+        a: "Ranchi",
+        b: "Delhi",
+        c: "Kashmir",
+        d: "Punjab",
+        ans: "ans2"
+    }
+]
+
+const question = document.querySelector('.question');
+const option1 = document.querySelector('#option1');
+const option2 = document.querySelector('#option2');
+const option3 = document.querySelector('#option3');
+const option4 = document.querySelector('#option4');
+const submit = document.querySelector('#submit');
+
+
+const answers = document.querySelectorAll('.answer');
+
+const showScore =document.querySelector('#showScore');
+
+let questionCount =0;
+let score=0;
+const loadQuestion =()=>{
+
+    const questionList = quizDB[questionCount];
+    
+
+
+    question.innerText = questionList.question;
+
+    option1.innerText = questionList.a;
+    option2.innerText = questionList.b;
+    option3.innerText = questionList.c;
+    option4.innerText = questionList.d;
+
+
+}
+
+
+
+loadQuestion();
+
+const getCheckAnswer = ()=> {
+    let answer;
+
+    answers.forEach((currAnsElem)=>{
+if(currAnsElem.checked){
+    answer =currAnsElem.id;
+  
+      }
+    });
+    return answer;
+};
+
+const deselectAll =()=>{
+    answers.forEach((currAnsElem)=>currAnsElem.checked = false);
+}
+
+submit.addEventListener('click',()=>{
+    const checkedAnswer = getCheckAnswer();
+
+    if(checkedAnswer == quizDB[questionCount].ans){
+        score++;
+    };
+ 
+    questionCount++;
+
+    deselectAll();
+    if(questionCount<quizDB.length){
+        loadQuestion();
+       
+    }else{
+showScore.innerHTML = `
+<h3>You scored ${score}/${quizDB.length}✌</h3>
+<button class = "btn" onclick = "location.reload()">Play Again </button>
+`;
+
+showScore.classList.remove('scoreArea');
+    }
+   
+});
